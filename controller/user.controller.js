1449ffc77fe181e3590e02efa707cd79561b2e45
Ddkotl <no64129@gmail.com>
2023-11-14 09:25:43 +0300

    password hash

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -1,18 +1,27 @@
+import bcrypt from 'bcrypt'
 import { validationResult } from 'express-validator'
 import { db } from '../database/db.js'
 class UserController {
 	async create(req, res) {
-		const errors = validationResult(req)
-		if (!errors.isEmpty()) {
-			return res.status(400).json(errors.array())
+		try {
+			const errors = validationResult(req)
+			if (!errors.isEmpty()) {
+				return res.status(400).json(errors.array())
+			}
+			const { name, email, password, avatarUrl } = req.body
+			const salt = await bcrypt.genSalt(10)
+			const passwordHash = await bcrypt.hash(password, salt)
+			const newUser = await db.query(
+				`INSERT INTO users (name,email,passwordHash,avatarUrl) values ($1,$2,$3,$4) RETURNING *`,
+				[name, email, passwordHash, avatarUrl]
+			)
+			res.json(newUser.rows)
+		} catch (err) {
+			console.log(err)
+			res.status(500).json({
+				message: 'Не удалось зарегестрироваться',
+			})
 		}
-		const { name, email, password, avatarUrl } = req.body
-		const passwordHash = password
-		const newUser = await db.query(
-			`INSERT INTO users (name,email,passwordHash,avatarUrl) values ($1,$2,$3,$4) RETURNING *`,
-			[name, email, passwordHash, avatarUrl]
-		)
-		res.json(newUser.rows)
 	}
 	async index(req, res) {
 		const users = await db.query(`SELECT * FROM users`)
